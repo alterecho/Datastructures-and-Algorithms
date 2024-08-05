8dfd0e500515a42b50702d05996bfe6b183d0d90
vijay <Vijaychandran.jayachandran@ntucenterprise.sg>
2024-08-05 14:06:44 +0800

    binary search recursive

--- DSA/Algorithms/BinarySearch.swift
+++ DSA/Algorithms/BinarySearch.swift
@@ -14,7 +14,7 @@ class BinarySearch<T: Comparable>: Search {
     self.elements = elements
   }
   
-  func find(_ target: T) -> Int? {
+  private func binarySearch_iterative(target: T) -> Int? {
     guard elements.count > 0 else {
       return nil
     }
@@ -32,7 +32,30 @@ class BinarySearch<T: Comparable>: Search {
         right = middle - 1
       }
     }
-    
     return nil
+  }
+  
+  private func binarySearch_recursive(target: T, elements: [T]? = nil) -> Int? {
+    let elements = elements ?? self.elements
+    guard elements.count > 0 else {
+      return nil
+    }
+    let index_mid = elements.count / 2
+    let element_mid = elements[index_mid]
+    if element_mid == target {
+      return index_mid
+    } else if target < element_mid && index_mid != 0 {
+      return binarySearch_recursive(target: target, elements: Array(elements[0..<index_mid]))
+    } else if target > element_mid && index_mid != elements.count - 1 {
+      guard let index = binarySearch_recursive(target: target, elements: Array(elements[index_mid..<elements.count])) else {
+        return nil
+      }
+      return index_mid + index
+    }
+    return nil
+  }
+
+  func find(_ target: T) -> Int? {
+    return binarySearch_recursive(target: target)
   }
 }
