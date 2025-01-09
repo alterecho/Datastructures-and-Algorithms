7dc666a25df88f5c29af70587892f83905320f00
Vijay <>
2025-01-09 16:42:03 +0800

    added Bench project

---
+++ Bench/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  asas
+//
+//  Created by Vijay on 1/9/25.
+//
+
+import Foundation
+import DSA
+
+let sort = MergeSort(elements: [5, 2, 4])
+print("Hello, World!")
+print(sort.elements)
+
