bd04a3bdc682541979cfd6b9bc839ff768e17388
Vijay <>
2025-01-09 16:41:28 +0800

    added HeapSort (Empty)

--- DSA/Algorithms/Sort/HeapSort.swift
+++ DSA/Algorithms/Sort/HeapSort.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+/*
+ Heap Sort (O(n log n) time, O(1) space): In-place sorting but not stable.
+ */
+class HeapSort<T: Comparable>: Sort {
+    var elements: [T]
+    required init(elements: [T]) {
+        self.elements = elements
+    }
+    
+    private func sort() {
+        
+    }
+}
